55c8115cfc5b289cfebf62fd807b2359694f25bd
fletcher-fletcher <translservicio@gmail.com>
2025-11-02 21:05:08 +0300

    Update Booking.jsx

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -59,7 +59,7 @@ const Booking = () => {
     setSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/appointments', {
+      const response = await fetch('https://car-detailing-app-14qu.onrender.com/api/appointments', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
